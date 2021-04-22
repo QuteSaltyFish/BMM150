4aac5a24ed262800aa43396760df0fd2c1d93e1d
Mingke Wang <qutesaltyfish@Mingkes-MacBook-Pro.local>
2021-04-22 19:12:31 +0800

    add spi implementation

--- examples/compass/compass.ino
+++ examples/compass/compass.ino
@@ -11,41 +11,41 @@
 BMM150 bmm = BMM150();
 
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    if (bmm.initialize() == BMM150_E_ID_NOT_CONFORM) {
-        Serial.println("Chip ID can not read!");
-        while (1);
-    } else {
-        Serial.println("Initialize done!");
-    }
-
+  if (bmm.begin_i2c() == BMM150_E_ID_NOT_CONFORM) {
+    Serial.println("Chip ID can not read!");
+    while (1)
+      ;
+  } else {
+    Serial.println("Initialize done!");
+  }
 }
 
 void loop() {
-    bmm150_mag_data value;
-    bmm.read_mag_data();
+  bmm150_mag_data value;
+  bmm.read_mag_data();
 
-    value.x = bmm.raw_mag_data.raw_datax;
-    value.y = bmm.raw_mag_data.raw_datay;
-    value.z = bmm.raw_mag_data.raw_dataz;
+  value.x = bmm.raw_mag_data.raw_datax;
+  value.y = bmm.raw_mag_data.raw_datay;
+  value.z = bmm.raw_mag_data.raw_dataz;
 
-    float xyHeading = atan2(value.x, value.y);
-    float zxHeading = atan2(value.z, value.x);
-    float heading = xyHeading;
+  float xyHeading = atan2(value.x, value.y);
+  float zxHeading = atan2(value.z, value.x);
+  float heading = xyHeading;
 
-    if (heading < 0) {
-        heading += 2 * PI;
-    }
-    if (heading > 2 * PI) {
-        heading -= 2 * PI;
-    }
-    float headingDegrees = heading * 180 / M_PI;
-    float xyHeadingDegrees = xyHeading * 180 / M_PI;
-    float zxHeadingDegrees = zxHeading * 180 / M_PI;
+  if (heading < 0) {
+    heading += 2 * PI;
+  }
+  if (heading > 2 * PI) {
+    heading -= 2 * PI;
+  }
+  float headingDegrees = heading * 180 / M_PI;
+  float xyHeadingDegrees = xyHeading * 180 / M_PI;
+  float zxHeadingDegrees = zxHeading * 180 / M_PI;
 
-    Serial.print("Heading: ");
-    Serial.println(headingDegrees);
+  Serial.print("Heading: ");
+  Serial.println(headingDegrees);
 
-    delay(100);
+  delay(100);
 }
